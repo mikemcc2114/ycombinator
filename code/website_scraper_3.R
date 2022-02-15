07bb7f42a5e143807440ced30fb324c9120d7fe3
mikemcc2114 <mike.mcc.2114@gmail.com>
2022-02-15 15:37:27 -0700

    fixed code to scrape XML and data from website. did 3 ways, with for loop, map_dfr, and possibly

---
+++ code/website_scraper_3.R
@@ -0,0 +1,64 @@
+library(tidyverse)
+library(xml2)
+library(rvest)
+library(here)
+library(purrr)
+library(dplyr)
+library(tidytext)
+library(wordcloud)
+library(ggplot2)
+library(textstem)
+
+# Y Combinator website sitemap
+sitemap <- "https://www.ycombinator.com/companies//sitemap.xml"
+pattern <- "https://www.ycombinator.com/companies/"
+
+# read sitemap XML and convert into dataframe and vector of company urls
+company_url <- sitemap %>% read_xml() %>% as_list() %>% 
+  as_tibble() %>% unnest_longer(urlset) %>% 
+  filter(urlset_id == "loc") %>% 
+  unnest(cols = names(.)) %>% unnest(urlset) %>% rename("url" = "urlset") 
+url_listing <- company_url$url
+
+# define paths for company name, season, and status tags
+path_name <- "/html/body/div/div[2]/div/div/div[1]/div[1]/div[2]/div[1]/h1"
+path_season <- "/html/body/div/div[2]/div/div/div[1]/div[1]/div[2]/div[2]/span[1]/text()"
+path_status <- "/html/body/div/div[2]/div/div/div[1]/div[1]/div[2]/div[2]/span[2]"
+
+# data scraped using for loop
+data <- tibble(name = character(), season = character(), status = character(),
+               headline = character(), description = character(), 
+               url = character())
+
+for(i in 1:10){
+  cat("Iteration", i, "out of", 10, "\n")
+  page <- read_html(url_listing[i])
+  name <- page %>% html_nodes(xpath = path_name) %>% html_text()
+  season <- page %>% html_nodes(xpath = path_season) %>% html_text()
+  status <- page %>% html_nodes(xpath = path_status) %>% html_text()
+  headline <- page %>% html_nodes("h3") %>% html_text() %>% .[1]
+  description <- page %>% html_nodes("p") %>% html_text() %>% .[1]
+  data <- data %>%  
+    add_row(name = name, season = season, status = status, headline = headline,
+            description = description,
+            url = url_listing[i])
+}
+
+# data scraped using map_dfr
+# define scraper function
+scraper <- function(url){
+  page <- read_html(url)
+  name <- page %>% html_nodes(xpath = path_name) %>% html_text()
+  season <- page %>% html_nodes(xpath = path_season) %>% html_text()
+  status <- page %>% html_nodes(xpath = path_status) %>% html_text()
+  headline <- page %>% html_nodes("h3") %>% html_text() %>% .[1]
+  description <- page %>% html_nodes("p") %>% html_text() %>% .[1]
+  tibble(name, season, status, headline, description, url)
+}
+
+# map scraper function to url listing vector (warning: takes 20+ min to download)
+data2 <- map_dfr(.x = url_listing[1:10], .f = scraper)
+
+# map scraper function to url_listing using "possibly" to catch failures
+possibly <- possibly(scraper, otherwise="This page could not be accessed")
+data3 <- map_dfr(.x = url_listing[1:10], .f = possibly)
